c4e217bec394474a81224891546a779de8cf2224
David Rosario <darosario3d@gmail.com>
2020-08-01 20:31:49 -0400

    palabra

---
+++ Controllers/DbController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Final.Controllers
+{
+    public class DbController : Controller{
+        public IActionResult Test(){
+                Console.WriteLine("Prueba webon");
+                return Redirect("/");
+        }
+    }
+}
