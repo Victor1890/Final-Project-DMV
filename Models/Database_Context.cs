23ba265eefdf6e3cd5709b991d9b54a37ef336ee
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-08-02 20:18:24 -0400

    Creando y migrando DataBase

---
+++ Models/Database_Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+    public class Database_Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var connectionbuilder = new SqliteConnectionStringBuilder { DataSource = "Db.db" };
+            var connectionString = connectionbuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+
+            builder.UseSqlite(connection);
+        }
+
+        public DbSet<Products> Products { get; set; }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Nombre_Produts { get; set; }
+
+        public double Precio_Produts { get; set; }
+
+        public byte[] Imagen_Produts { get; set; }
+
+        public int Num_Products { get; set; }
+    }
+}
