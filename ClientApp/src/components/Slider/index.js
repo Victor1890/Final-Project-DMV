5abd0ff4bad1c17e87c4e4c17fcdca7e0564ef37
David Rosario <darosario3d@gmail.com>
2020-08-05 13:56:51 -0400

    Slider

---
+++ ClientApp/src/components/Slider/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+ export function Slider (){
+    return(
+<div id="carouselExampleSlidesOnly" class="carousel slide mt-2" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-100" src="https://s1.eestatic.com/2020/03/19/ciencia/nutricion/Alimentacion-Dieta-Obesidad-Coronavirus-Nutricion_475962417_148657032_1706x1280.jpg" alt="First slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://i.blogs.es/0baa91/39f95492-38ee-4f16-92e1-ba5161f4a958/450_1000.jpeg" alt="Second slide"/>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.65ymas.com/uploads/s1/19/47/57/pros-y-contras-de-hacer-la-compra-del-supermercado-por-internet.jpeg" alt="Third slide"/>
+    </div>
+  </div>
+</div>
+
+    );
+}
--- ClientApp/src/pages/Home.js
+++ ClientApp/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Contact from "./Contact";
 import Team from "./Team/Team";
-
+import {Slider} from "./../components/Slider/index";
 export default function Home() {
   return (
     <div className="App">
@@ -23,7 +23,7 @@ export default function Home() {
           </div>
         </div>
       </header>
-
+        <Slider/>
       <section className="page-section" id="services">
         <div className="container">
           <div className="row">
