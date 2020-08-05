dffe54429b179944f3551d86b2edbed96d6b0076
David Rosario <darosario3d@gmail.com>
2020-08-05 09:46:34 -0400

    Promise en Products.js

--- ClientApp/src/pages/Produts/Products.js
+++ ClientApp/src/pages/Produts/Products.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Item_Products from "../../components/Produts";
 
+fetch('/api/ApiProducts')
+.then(data => {return data.json()})
+.then(pro => {console.log(pro)});
+
+
 export function Products() {
   return (
     <>
