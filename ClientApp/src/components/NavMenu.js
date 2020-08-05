4421eb466d5898c2d082ea6cfc956ce6a890cca0
Victor J. Rosario V <victorrosario.1890@gmail.com>
2020-08-05 14:26:15 -0400

    a

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,90 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { CircleButton } from './Button';
-import './NavMenu.css';
-import { Users, GetDataUser } from '../Services';
+import React from "react";
+import { Link } from "react-router-dom";
+import { CircleButton } from "./Button";
+import "./NavMenu.css";
 
-export function NavMenu () {
-
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    Users(GetDataUser({email, pass}))
-    .then(res => {
-      const {email, pass} = res.data;
-      setUser(GetDataUser({email, pass}));
-    })
-  },[]);
-
+export function NavMenu() {
   return (
     <>
-      {user == null ? (<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-      <div className="container">
-        <Link className="navbar-brand js-scroll-trigger" to='/'>DMV Mercado</Link>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav text-uppercase ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/'>Inicio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/Products'>Productos</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/About'>Sobre Nosotros</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/Contact' className="nav-link js-scroll-trigger">Contactos</Link>
-            </li>
-            <li className="nav-item cta cta-colored">
-              <Link to='' className="nav-link">
-                <i className="fa fa-shopping-cart"></i>
-                [0]
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top"
+        id="mainNav"
+      >
+        <div className="container">
+          <Link className="navbar-brand js-scroll-trigger" to="/">
+            DMV Mercado
+          </Link>
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            Menu
+            <i className="fa fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav text-uppercase ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link js-scroll-trigger" to="/">
+                  Inicio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link js-scroll-trigger" to="/Products">
+                  Productos
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link js-scroll-trigger" to="/About">
+                  Sobre Nosotros
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Contact" className="nav-link js-scroll-trigger">
+                  Contactos
+                </Link>
+              </li>
+              <li className="nav-item cta cta-colored">
+                <Link to="" className="nav-link">
+                  <i className="fa fa-shopping-cart"></i>
+                  [0]
+                </Link>
+              </li>
+              <Link to="/Login">
+                <CircleButton
+                  classnameIcon="fa fa-user"
+                  classnameButton="nav-link js-scroll-trigger btn btn-circle"
+                ></CircleButton>
               </Link>
-            </li>
-            <Link to='/Login'>
-                <CircleButton classnameIcon='fa fa-user' classnameButton='nav-link js-scroll-trigger btn btn-circle'></CircleButton>
-              </Link>
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>) : (<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-      <div className="container">
-        <Link className="navbar-brand js-scroll-trigger" to='/'>DMV Mercado</Link>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fa fa-bars"></i>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav text-uppercase ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/'>Inicio</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/Products'>Productos</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link js-scroll-trigger" to='/About'>Sobre Nosotros</Link>
-            </li>
-            <li className="nav-item">
-              <Link to='/Contact' className="nav-link js-scroll-trigger">Contactos</Link>
-            </li>
-            <li className="nav-item cta cta-colored">
-              <Link to='' className="nav-link">
-                <i className="fa fa-shopping-cart"></i>
-                [0]
-              </Link>
-            </li>
-            <Link to='/Login'>
-              <a className="nav-link js-scroll-trigger">{user.name}</a>
-            </Link>
-          </ul>
-        </div>
-      </div>
-    </nav>)}
+      </nav>
     </>
   );
 }
